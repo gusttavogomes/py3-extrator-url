4613fe7e03c78017f4e34d217a5d88e95a43c9f0
gusttavogomes <30482020+gusttavogomes@users.noreply.github.com>
2023-02-13 08:46:48 -0300

    Update extrator_url.py
    Finalizando o extrator de informações de uma URL.

--- extrator_url.py
+++ extrator_url.py
@@ -1,10 +1,17 @@
 import re
-
-
 class ExtratorURL:
     def __init__(self, url):
         self.url = self.sanitiza_url(url)
         self.valida_url()
+
+    def __len__(self):
+        return len(self.url)
+
+    def __str__(self):
+        return self.url + "\n" + "Parâmetros: " + self.get_url_parametros() + "\n" + "URL Base: " + self.get_url_base()
+
+    def __eq__(self, other):
+        return self.url == other.url
 
     def sanitiza_url(self, url):
         if type(url) == str:
@@ -43,7 +50,19 @@ class ExtratorURL:
         return valor
 
 
+# Exemplos de teste
+extrator_url = ExtratorURL("https://bytebank.com/cambio?moedaOrigem=real&moedaDestino=dolar&quantidade=100") # Exemplo R$ pra $
+# extrator_url = ExtratorURL("https://bytebank.com/cambio?moedaOrigem=dolar&moedaDestino=real&quantidade=100") # Exemplo $ pra R$
 
-extrator_url = ExtratorURL("https://bytebank.com/cambio?moedaOrigem=real&moedaDestino=dolar&quantidade=100")
-valor_quantidade = extrator_url.get_valor_parametro("quantidade")
-print(valor_quantidade)
+VALOR_DOLAR = 5.50  # 1 dólar = 5.50 reais
+moeda_origem = extrator_url.get_valor_parametro("moedaOrigem")
+moeda_destino = extrator_url.get_valor_parametro("moedaDestino")
+quantidade = extrator_url.get_valor_parametro("quantidade")
+
+if moeda_origem == 'real' and moeda_destino == 'dolar':
+    print(f"O valor de R$ {quantidade} reais, é igual a $ {format(float(quantidade)/VALOR_DOLAR,'.2f')} dólares")
+elif moeda_origem == 'dolar' and moeda_destino == 'real':
+    print(f"O valor de $ {quantidade} dólares, é igual a R$ {format(float(quantidade) * VALOR_DOLAR,'.2f')} reais")
+    # print(float(quantidade) * VALOR_DOLAR)
+else:
+    print(f"A conversão de {moeda_origem} para {moeda_destino} não está disponível")
